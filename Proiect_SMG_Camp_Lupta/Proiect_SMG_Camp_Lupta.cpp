576f7d77d6b71982b1e95728aa9cb3f82a4baa2a
Rusu-Stefan-Ionut <stefanrusu15@yahoo.com>
2022-12-21 03:27:37 +0200

    Organized code in separated classed, added Texture class and ShaderClass

--- Proiect_SMG_Camp_Lupta/Proiect_SMG_Camp_Lupta.cpp
+++ Proiect_SMG_Camp_Lupta/Proiect_SMG_Camp_Lupta.cpp
@@ -17,6 +17,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
@@ -26,14 +27,15 @@
 #pragma comment (lib, "OpenGL32.lib")
 //#include<glad/glad.h>
 
-/*struct Vertex
+struct Vertex
 {
 	glm::vec3 position;
 	glm::vec3 normal;
 	glm::vec3 color;
 	glm::vec2 texUV;
-};*/
-/*class EBO
+};
+
+class EBO
 {
 public:
 	// ID reference of Elements Buffer Object
@@ -51,7 +53,6 @@ public:
 	{
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ID);
 	}
-	}
 	// Unbinds the EBO
 	void Unbind()
 	{
@@ -62,8 +63,8 @@ public:
 	{
 		glDeleteBuffers(1, &ID);
 	}
-}; */
-/*class VBO
+}; 
+class VBO
 {
 public:
 	// Reference ID of the Vertex Buffer Object
@@ -91,8 +92,8 @@ public:
 
 		glDeleteBuffers(1, &ID);
 	}
-};*/
-/*class VAO
+};
+class VAO
 {
 public:
 	// ID reference for the Vertex Array Object
@@ -125,13 +126,195 @@ public:
 	void Delete() {
 		glDeleteVertexArrays(1, &ID);
 	}
-}; */
+}; 
 
-/*class Mesh
+std::string get_file_contents(const char* filename)
+{
+	std::ifstream in(filename, std::ios::binary);
+	if (in)
+	{
+		std::string contents;
+		in.seekg(0, std::ios::end);
+		contents.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&contents[0], contents.size());
+		in.close();
+		return(contents);
+	}
+	throw(errno);
+}
+
+class Shader
+{
+public:
+	// Reference ID of the Shader Program
+	GLuint ID;
+	// Constructor that build the Shader Program from 2 different shaders
+	Shader(const char* vertexFile, const char* fragmentFile)
+	{
+		// Read vertexFile and fragmentFile and store the strings
+		std::string vertexCode = get_file_contents(vertexFile);
+		std::string fragmentCode = get_file_contents(fragmentFile);
+
+		// Convert the shader source strings into character arrays
+		const char* vertexSource = vertexCode.c_str();
+		const char* fragmentSource = fragmentCode.c_str();
+
+		// Create Vertex Shader Object and get its reference
+		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		// Attach Vertex Shader source to the Vertex Shader Object
+		glShaderSource(vertexShader, 1, &vertexSource, NULL);
+		// Compile the Vertex Shader into machine code
+		glCompileShader(vertexShader);
+		// Checks if Shader compiled succesfully
+		compileErrors(vertexShader, "VERTEX");
+
+		// Create Fragment Shader Object and get its reference
+		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		// Attach Fragment Shader source to the Fragment Shader Object
+		glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+		// Compile the Vertex Shader into machine code
+		glCompileShader(fragmentShader);
+		// Checks if Shader compiled succesfully
+		compileErrors(fragmentShader, "FRAGMENT");
+
+		// Create Shader Program Object and get its reference
+		ID = glCreateProgram();
+		// Attach the Vertex and Fragment Shaders to the Shader Program
+		glAttachShader(ID, vertexShader);
+		glAttachShader(ID, fragmentShader);
+		// Wrap-up/Link all the shaders together into the Shader Program
+		glLinkProgram(ID);
+		// Checks if Shaders linked succesfully
+		compileErrors(ID, "PROGRAM");
+
+		// Delete the now useless Vertex and Fragment Shader objects
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+	}
+
+	// Activates the Shader Program
+	void Activate()
+	{
+		glUseProgram(ID);
+	}
+	// Deletes the Shader Program
+	void Delete()
+	{
+		glDeleteProgram(ID);
+	}
+private:
+	// Checks if the different Shaders have compiled properly
+	void compileErrors(unsigned int shader, const char* type)
+	{
+		// Stores status of compilation
+		GLint hasCompiled;
+		// Character array to store error message in
+		char infoLog[1024];
+		if (type != "PROGRAM")
+		{
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &hasCompiled);
+			if (hasCompiled == GL_FALSE)
+			{
+				glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "SHADER_COMPILATION_ERROR for:" << type << "\n" << infoLog << std::endl;
+			}
+		}
+		else
+		{
+			glGetProgramiv(shader, GL_LINK_STATUS, &hasCompiled);
+			if (hasCompiled == GL_FALSE)
+			{
+				glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+				std::cout << "SHADER_LINKING_ERROR for:" << type << "\n" << infoLog << std::endl;
+			}
+		}
+	}
+};
+
+class Texture
+{
+public:
+	GLuint ID;
+	const char* type;
+	GLuint unit;
+
+	Texture(const char* image, const char* texType, GLuint slot, GLenum format, GLenum pixelType)
+	{
+		// Assigns the type of the texture ot the texture object
+		type = texType;
+
+		// Stores the width, height, and the number of color channels of the image
+		int widthImg, heightImg, numColCh;
+		// Flips the image so it appears right side up
+		stbi_set_flip_vertically_on_load(true);
+		// Reads the image from a file and stores it in bytes
+		unsigned char* bytes = stbi_load(image, &widthImg, &heightImg, &numColCh, 0);
+
+		// Generates an OpenGL texture object
+		glGenTextures(1, &ID);
+		// Assigns the texture to a Texture Unit
+		glActiveTexture(GL_TEXTURE0 + slot);
+		unit = slot;
+		glBindTexture(GL_TEXTURE_2D, ID);
+
+		// Configures the type of algorithm that is used to make the image smaller or bigger
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		// Configures the way the texture repeats (if it does at all)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		// Extra lines in case you choose to use GL_CLAMP_TO_BORDER
+		// float flatColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
+		// glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flatColor);
+
+		// Assigns the image to the OpenGL Texture object
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, format, pixelType, bytes);
+		// Generates MipMaps
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		// Deletes the image data as it is already in the OpenGL Texture object
+		stbi_image_free(bytes);
+
+		// Unbinds the OpenGL Texture object so that it can't accidentally be modified
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+
+	// Assigns a texture unit to a texture
+	void texUnit(Shader& shader, const char* uniform, GLuint unit)
+	{
+		// Gets the location of the uniform
+		GLuint texUni = glGetUniformLocation(shader.ID, uniform);
+		// Shader needs to be activated before changing the value of a uniform
+		shader.Activate();
+		// Sets the value of the uniform
+		glUniform1i(texUni, unit);
+	}
+	// Binds a texture
+	void Bind()
+	{
+		glActiveTexture(GL_TEXTURE0 + unit);
+		glBindTexture(GL_TEXTURE_2D, ID);
+	}
+	// Unbinds a texture
+	void Unbind()
+	{
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+	// Deletes a texture
+	void Delete()
+	{
+		glDeleteTextures(1, &ID);
+	}
+};
+
+class Mesh
 {
 public:
 	std::vector <Vertex> vertices;
-	std::vector <GLunit> indices;
+	std::vector <GLuint> indices;
 	std::vector <Texture> textures;
 
 	VAO VAO;
@@ -190,7 +373,8 @@ public:
 		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
 	}
 
-};*/
+};
+
 bool spin;
 
 const GLfloat SKY_BLUE[] = { 0.5, 0.7, 1.0, 1.0 };
---
+++ Proiect_SMG_Camp_Lupta/Texture.h
@@ -0,0 +1,28 @@
+#ifndef TEXTURE_CLASS_H
+#define TEXTURE_CLASS_H
+
+#include <GL/glew.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include"ShaderClass.h"
+
+class Texture
+{
+public:
+	GLuint ID;
+	const char* type;
+	GLuint unit;
+
+	Texture(const char* image, const char* texType, GLuint slot, GLenum format, GLenum pixelType);
+
+	// Assigns a texture unit to a texture
+	void texUnit(Shader2& shader, const char* uniform, GLuint unit);
+	// Binds a texture
+	void Bind();
+	// Unbinds a texture
+	void Unbind();
+	// Deletes a texture
+	void Delete();
+};
+#endif
